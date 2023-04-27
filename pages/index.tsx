ec6a2874ff8dc28c761d188f46de24793bf2e5ce
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2023-04-27 22:24:55 +0530

    :bug: fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,65 +5,67 @@ import { providers, Contract } from 'ethers';
 import { useState, useEffect, useRef } from 'react';
 import { CONTRACT_ADDRESS, abi } from '../constants';
 import Link from 'next/link';
-import blockchain from "../public/blockchain.png"
+import blockchain from '../public/blockchain.png';
 
 export default function Home() {
-  // const web3ModalRef = useRef();
+  const web3ModalRef = useRef();
 
-  // const getProviderOrSigner = async (needSigner = false) => {
-  //   console.log(web3ModalRef);
-  //   const provider = await web3ModalRef.current.connect();
-  //   const web3Provider = new providers.Web3Provider(provider);
+  const getProviderOrSigner = async (needSigner = false) => {
+    console.log(web3ModalRef);
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
 
-  //   const network = await web3Provider.getNetwork();
-  //   if (network.chainId !== 5) {
-  //     window.alert('Change your network to goerli testnet');
-  //     throw new Error('Change to goerli testnet');
-  //   }
+    const network = await web3Provider.getNetwork();
+    if (network.chainId !== 5) {
+      window.alert('Change your network to goerli testnet');
+      throw new Error('Change to goerli testnet');
+    }
 
-  //   if (needSigner) {
-  //     const signer = web3Provider.getSigner();
-  //     return signer;
-  //   }
-  //   return web3Provider;
-  // };
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
 
-  // const allProjects = async () => {
-  //   try {
-  //     const provider = await getProviderOrSigner();
-  //     const contract = new Contract(CONTRACT_ADDRESS, abi, provider);
-  //     const projID = await contract.receiveProjectid();
-  //     console.log(projID._hex);
-  //     let projectInfo = [];
-  //     for (let i = 0; i < projID; i++) {
-  //       const project = await contract.projectlist(i);
+  const allProjects = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const contract = new Contract(CONTRACT_ADDRESS, abi, provider);
+      const projID = await contract.receiveProjectid();
+      console.log(projID._hex);
+      let projectInfo = [];
+      for (let i = 0; i < projID; i++) {
+        const project = await contract.projectlist(i);
 
-  //       projectInfo.push(project);
-  //     }
-  //     console.log(projectInfo);
-  //     return projectInfo;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+        projectInfo.push(project);
+      }
+      console.log(projectInfo);
+      return projectInfo;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // useEffect(() => {
-  //   web3ModalRef.current = new Web3Modal({
-  //     network: 'goerli',
-  //     providerOptions: {},
-  //     disableInjectedProvider: false,
-  //   });
-  //   const data = allProjects();
-  // }, []);
+  useEffect(() => {
+    web3ModalRef.current = new Web3Modal({
+      network: 'goerli',
+      providerOptions: {},
+      disableInjectedProvider: false,
+    });
+    const data = allProjects();
+  }, []);
 
   return (
     <>
       <Menu />
       <div className="flex justify-center item-center">
-        <div className='h-screen w-screen'>
-          <img src="/blockchain.gif" alt="" className='h-full w-full'/>
+        <div className="h-screen w-screen">
+          <img src="/blockchain.gif" alt="" className="h-full w-full" />
         </div>
-        <h3 className="absolute text-white md:font-bold mt-60  text-9xl">VotEase</h3>
+        <h3 className="absolute text-white md:font-bold mt-60  text-9xl">
+          VotEase
+        </h3>
       </div>
     </>
   );
